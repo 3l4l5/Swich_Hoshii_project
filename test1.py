67a4a03a87e7e42b3e21bb1f4b95567eeb0fb8c0
citron-Win <csri16021@g.nihon-u.ac.jp>
2020-06-14 18:04:11 +0900

    20200614

--- test1.py
+++ test1.py
@@ -2,12 +2,12 @@ import requests as req
 from bs4 import BeautifulSoup
 import re
 import datetime
+import time
 
 
 
 
-
-line_access_token = 'あなたのアクセストークン'
+line_access_token = 'hlGm1bP3PRTu1wyyZgM99zbanw1QfX8oWA71vHG4hki'
 
 
 
@@ -40,29 +40,39 @@ pink = "https://books.rakuten.co.jp/rb/16247998/?bkts=1&l-id=search-c-item-text-
 
 #リストにまとめる
 products = [big,gray,yello,blue,pink]
+#products = [pro_con,gray,yello,blue,pink]
 products_name = ["Nintendo Swich 本体","Nintendo Swich lite(グレー)","Nintendo Swich lite(イエロー)","Nintendo Swich lite(グレー)","Nintendo Swich lite(ブルー)","Nintendo Swich lite(ピンク)"]
 
 #無間ループ スクレイピングの規約に違反するかわからないけど、結構処理に時間かかって1分置きくらいにしかスクレイピングできないからまあ、許してほしい
 while True:
     print(str(datetime.datetime.now())+"\n"+"ｳｺﾞｲﾃﾙﾖ!\n")
-    arumono = []
-    for a in products:
-        if Can_buy(a):
-            arumono.append(True)
-        else:
-            arumono.append(False)
 
-    message = []
-    dore = 0
-    for b in arumono:
-        if b:
-            message.append(products_name[dore]+"の在庫があります URL:"+products[dore])
-            dore += 1
+    try:
+        arumono = []
+        for a in products:
+            if Can_buy(a):
+                arumono.append(True)
+            else:
+                arumono.append(False)
 
-    message_str = "\n"
-    for c in message:
-        message_str += c
-        message_str += "\n"
+        message = []
+        dore = 0
+        for b in arumono:
+            if b:
+                message.append(products_name[dore]+"の在庫があります URL:"+products[dore])
+                dore += 1
 
-    sendmessage(message_str[:-1])
-#無間ループ内中、動てるかわからないのでその時の時間と一緒に出力するようにした
+        message_str = "\n"
+        for c in message:
+            message_str += c
+            message_str += "\n"
+        sendmessage(message_str[:-1])
+    #無間ループ内中、動てるかわからないのでその時の時間と一緒に出力するようにした
+
+
+    #エラーが出た場合の処理を記載
+    except Exception as e:
+        sendmessage("ｴﾗｰｶﾞﾃﾞﾀﾖ!ｴﾗｰﾅｲﾖｳ↓")
+        sendmessage(e)
+        sendmessage("1分待って再チャレンジします")
+        time.sleep(60)
